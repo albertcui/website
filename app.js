713524b78d8d1f3ce6297089fd65672660a76cd7
Albert Cui <aqc2109@columbia.edu>
2017-02-23 01:35:06 +0000

    remove reddits

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ app.use("/public", express.static(path.join(__dirname, '/public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade');
 
-var subreddits;
+// var subreddits;
 
-r.login(function(){
-	r.getAllSubreddits(function(err, data){
-		if(err) throw err
-		else {
-			subreddits = data
-            subreddits.sort(sorter)
-		}
-	})
-})
+// r.login(function(){
+// 	r.getAllSubreddits(function(err, data){
+// 		if(err) throw err
+// 		else {
+// 			subreddits = data
+//             subreddits.sort(sorter)
+// 		}
+// 	})
+// })
 
-function sorter(a, b) {
-    a = a.toLowerCase()
-    b = b.toLowerCase()
-    if (a > b) return 1
-    if (a < b) return -1
-    return 0
-}
+// function sorter(a, b) {
+//     a = a.toLowerCase()
+//     b = b.toLowerCase()
+//     if (a > b) return 1
+//     if (a < b) return -1
+//     return 0
+// }
 
 app.route('/').get(function(req, res){
 	res.render(
-        'index.jade',
-        {
-            subs: subreddits
-        }
+        'index.jade'//,
+        // {
+        //     subs: subreddits
+        // }
     )
 })
 
 
-var port = 5000;
+var port = 2000;
 
 app.listen(port, function() {
     console.log("Listening on " + port);
