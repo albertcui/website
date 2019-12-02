8bff5628571664f670aaee2370103d5695c697af
Albert Cui <aqc2109@columbia.edu>
2019-12-01 22:26:23 -0500

    try gatsby

---
+++ public/component---src-pages-index-js-be386a3c887666b3f22a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),r=t.n(a),l=t("Bl7J"),c=t("vrFN");n.default=function(){return r.a.createElement(l.a,null,r.a.createElement(c.a,{title:"Home"}),r.a.createElement("h1",null,"Work in progress"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-be386a3c887666b3f22a.js.map
